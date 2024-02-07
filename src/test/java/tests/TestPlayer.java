5e732a4f9cd72cf2144ff6f7a6c2ac2d8c28895e
sannidhi hegde <hsannidhi@gmail.com>
2024-02-06 20:11:00 -0500

    updated with testng

--- src/test/java/tests/TestPlayer.java
+++ src/test/java/tests/TestPlayer.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import pages.BaseClass;
@@ -10,26 +11,32 @@ import pages.WelcomeScreenActions;
 
 public class TestPlayer extends BaseClass{
 	
+	WelcomeScreenActions welcomePage = new WelcomeScreenActions();
+	ChooseFavoriteLeague chooseFavoriteLeague = new ChooseFavoriteLeague();
+	Standings standings = new Standings();
+	Utils utils = new Utils();
+	
+	
+	
+	@Parameters({"tapFavoriteLeague","favoriteLeagueInStandingsTab","pageTitle","findTeamName"})
 	@Test
-	public void testWelcomeScreen() throws InterruptedException{
-		
-		WelcomeScreenActions welcomePage = new WelcomeScreenActions();
-		ChooseFavoriteLeague chooseFavoriteLeague = new ChooseFavoriteLeague();
-		Standings standings = new Standings();
-		Utils utils = new Utils();
+	public void testTeamNameInStandingsTab(String tapFavoriteLeagueValue, String TapFavoriteLeagueValue, String pageTitleValue, String findTeamNameValue) throws InterruptedException{
+		//Test Welcome screen
 		welcomePage.testWelcomeScreen();
+		//Choose a Favorite League during on-boaring
 		chooseFavoriteLeague.verifyFavoriteLeaguePage();
-		chooseFavoriteLeague.tapOnLeagueByName("NHL Hockey");
+		chooseFavoriteLeague.tapOnLeagueByName(tapFavoriteLeagueValue);
+		//Navigate to Favorites home page
 		utils.clickContinueOrDoneButton();
 		utils.allowLocationPermission();
 		utils.clickContinueOrDoneButton();
 		utils.clickContinueOrDoneButton();
 		utils.dismissPopup();
-		standings.tapFavoriteLeague("NHL");
+		//Verify if the given team name is present in a list of team names in Standings tab
+		standings.tapFavoriteLeague(TapFavoriteLeagueValue);
 		standings.tapStandingstab();
-		utils.verifyPageTitle("NHL");
-		System.out.println("Title of the page verified");
-		standings.isTeamNamePresent("BOS Bruins");
+		utils.verifyPageTitle(pageTitleValue);
+		standings.isTeamNamePresent(findTeamNameValue);
 		utils.goBack();
 		standings.verifyFavoritesHomeScreen();
 	}
